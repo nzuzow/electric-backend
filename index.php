84efd9f2cbadf35a761b5018348bc4b24678f1b4
Nick <zuzownic@msu.edu>
2014-07-31 01:43:51 -0400

    added some basic jquery

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+<! DOCTYPE html>
 <?php
 	header("Access-Control-Allow-Origin: http://independentelectrical.net");
+?>
+<html>
+<head>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script>
+		$(document).ready(function() {
+				window.location = "http://independentelectrical.net/contact.php";
+		});
+	</script>
+</head>
+<?php
 	$name = $_POST["contact-name"];
 	echo $name."<br/>";
 	echo "Hello World!";
 ?>
+</html>
